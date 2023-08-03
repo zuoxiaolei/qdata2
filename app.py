c7e2a2e5f323cf9849e9028ad23719301d2cc55e
zuoxiaolei <1905561110@qq.com>
2023-08-04 01:06:08 +0800

    fix app.py

--- app.py
+++ app.py
@@ -2,93 +2,77 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+from mysql_util import get_connection, get_max_date
 
 is_local = False
 ttl = 600
 height = 740
 width = 800
-index_name = 'slope_standard'
-if is_local:
-    qdata_prefix = "data/"
-    ttl = 0
-else:
-    qdata_prefix = "https://ghproxy.com/https://raw.githubusercontent.com/zuoxiaolei/qdata/main/data/"
-
-
-@st.cache_resource(ttl=ttl)
-def load_stock_data():
-    df = pd.read_csv(qdata_prefix + 'rsrs.csv', dtype={'code': object})
-    return df
-
-
-@st.cache_resource(ttl=ttl)
-def load_fund_data():
-    df = pd.read_csv(qdata_prefix + 'rsrs_fund.csv', dtype={'code': object})
-    return df
-
-
-@st.cache_resource(ttl=ttl)
-def load_etf_strategy():
-    df = pd.read_csv(qdata_prefix + 'etf_strategy.csv', dtype={'code': object})
-    return df
-
-
-@st.cache_resource(ttl=ttl)
-def load_best_parameter():
-    best_params = pd.read_csv(qdata_prefix + 'best_params.csv', dtype={'code': object})
-    return best_params
-
-
-@st.cache_resource(ttl=ttl)
-def load_etf_basic_info():
-    df = pd.read_csv(qdata_prefix + "dim/exchang_eft_basic_info.csv", dtype={'基金代码': object})
-    df = df[['基金代码', '基金简称']]
-    df.columns = ['code', 'name']
-    return df
-
-
-stock_df = load_stock_data()
-fund_df = load_fund_data()
-etf_strategy_df = load_etf_strategy()
-best_params = load_best_parameter()
-etf_basic_info = load_etf_basic_info()
-etf_strategy_df = etf_strategy_df.merge(etf_basic_info, on="code")
-
-col1, col2 = st.columns(2)
-with col1:
-    code1 = st.text_input('股代码/ETF基金代码', '159915')
-    select_stock_df = stock_df[stock_df.code == code1]
-    st.dataframe(select_stock_df, height=height, hide_index=True, width=width)
-    if len(select_stock_df) > 0:
-        x = [datetime.datetime.strptime(ele, '%Y-%m-%d') for ele in select_stock_df.date]
-        y = select_stock_df[index_name]
-        plt.plot(x, y)
-        plt.title(code1)
-        st.pyplot(plt.gcf())
-
-with col2:
-    code2 = st.text_input('支付宝基金代码', '162605')
-    select_fund_df = fund_df[fund_df.code == code2]
-    st.dataframe(select_fund_df, height=height, hide_index=True, width=width)
-    if len(select_fund_df) > 0:
-        x = [datetime.datetime.strptime(ele, '%Y-%m-%d') for ele in select_fund_df.date]
-        y = select_fund_df[index_name]
-        plt.figure()
-        plt.plot(x, y)
-        plt.title(code2)
-        st.pyplot(plt.gcf())
+index_name = 'slope'
+columns = ['code', 'name', 'scale',
+           'date', 'price', 'slope',
+           'last_slope', 'slope_low',
+           'slop_high', 'signal']
+code = st.text_input('股代码/ETF基金代码', '159915')
+max_date = get_max_date(n=1)
+with get_connection() as cursor:
+    sql = f'''
+    select *
+    from (
+    select *
+    from etf.ads_etf_strategy_history_rpt
+    where code='{code}' and date<'{max_date}'
+    union all 
+    select *
+    from etf.ads_etf_strategy_rt_rpt
+    where code='{code}' and date='{max_date}'
+    ) t 
+    order by date desc
+    limit 20
+    '''
+    cursor.execute(sql)
+    select_stock_df = cursor.fetchall()
+select_stock_df = pd.DataFrame(select_stock_df, columns=columns)
+select_stock_df = select_stock_df.drop(['last_slope', 'signal'], axis=1)
+st.dataframe(select_stock_df, height=height, hide_index=True, width=width)
+if len(select_stock_df) > 0:
+    x = [datetime.datetime.strptime(ele, '%Y-%m-%d') for ele in select_stock_df.date]
+    y = select_stock_df[index_name]
+    plt.plot(x, y)
+    plt.xticks(rotation=45)
+    plt.title(code)
+    st.pyplot(plt.gcf())
 
 st.markdown("## 自选股票/基金")
-self_select_codes = ['159941', '512000', '159915',
-                     '512880', '603986', '515800',
-                     '512100', '510300', '513050', '510210', '512760']
-self_select_df = stock_df[stock_df.code.isin(self_select_codes)].groupby('code').tail(1)
+with get_connection() as cursor:
+    sql = '''
+    select *
+    from etf.ads_etf_strategy_rt_rpt
+    where date in (select date from etf.dim_etf_trade_date where rn=1) 
+          and code in (select code from ads_etf_subscribe)
+              and slope_low is not null
+    order by scale desc
+    '''
+    cursor.execute(sql)
+    self_select_df = cursor.fetchall()
+self_select_df = pd.DataFrame(self_select_df, columns=columns)
 self_select_df = self_select_df.sort_values(index_name)
-self_select_df = self_select_df.merge(best_params, on=['code'])
 st.dataframe(self_select_df, height=390, hide_index=True)
 
 st.markdown("## rsrs策略推荐")
-buy_df = etf_strategy_df[etf_strategy_df.signal == 'buy']
-sell_df = etf_strategy_df[etf_strategy_df.signal == 'sell']
+with get_connection() as cursor:
+    sql = '''
+    select *
+    from etf.ads_etf_strategy_rt_rpt
+    where date in (select date from etf.dim_etf_trade_date where rn=1) 
+              and slope_low is not null
+                and `signal` is not null
+    order by scale desc
+    '''
+    cursor.execute(sql)
+    buy_sell_df = cursor.fetchall()
+buy_sell_df = pd.DataFrame(buy_sell_df, columns=columns)
+buy_df = buy_sell_df[buy_sell_df.signal == 'buy']
+sell_df = buy_sell_df[buy_sell_df.signal == 'sell']
 st.dataframe(buy_df, hide_index=True)
 st.dataframe(sell_df, hide_index=True)
