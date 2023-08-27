b4e0d42c0f7e506f2f62c96fdb86f616d88146c2
zuoxiaolei <1905561110@qq.com>
2023-08-27 18:36:55 +0800

    add flowin strategy

--- money_flow.py
+++ money_flow.py
@@ -1,14 +1,16 @@
+import tqdm
 import tushare as ts
-from mysql_util import  insert_table_by_batch, get_connection
+from mysql_util import insert_table_by_batch, get_connection
 from concurrent.futures import ThreadPoolExecutor
 
 ts.set_token('c911eca61b662e07f7f313603fe50193de90d9ff791c0df63f67196f')
 pro = ts.pro_api()
 
+
 def get_stock_basic_info():
     data = pro.query('stock_basic', exchange='',
-                      list_status='L',
-                      fields='ts_code,symbol,name,area,industry,list_status,list_date')
+                     list_status='L',
+                     fields='ts_code,symbol,name,area,industry,list_status,list_date')
     data = data.values.tolist()
     sql = '''
     replace into tushare.stock_basic
@@ -21,13 +23,14 @@ def get_stock_money_flow(ts_code):
     start_date = '19900101'
     end_date = '29990101'
     df = pro.moneyflow(ts_code=ts_code,
-                       start_date=start_date, 
+                       start_date=start_date,
                        end_date=end_date)
     sql = '''
     replace into tushare.moneyflow
     values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
     '''
     insert_table_by_batch(sql, df.values.tolist())
+
 
 def get_all_stock_money_flow():
     sql = '''
@@ -40,4 +43,31 @@ def get_all_stock_money_flow():
     with ThreadPoolExecutor(10) as executor:
         executor.map(get_stock_money_flow, ts_codes)
 
-get_all_stock_money_flow()
+
+def get_zhongzheng1000_history():
+    get_date_sql = """
+    select substr(replace(date, '-', ''),1,6) date
+    from etf.dim_etf_trade_date
+    group by substr(replace(date, '-', ''),1,6)
+    order by date desc
+    """
+    zhongzheng1000_code = "000852.SH"
+    with get_connection() as cursor:
+        cursor.execute(get_date_sql)
+        data = cursor.fetchall()
+    date_all = [ele[0] for ele in data]
+    for date in tqdm.tqdm(date_all):
+        start_date = date[:-2] + "01"
+        end_date = date[:-2] + "31"
+        print(date)
+        df = pro.index_weight(index_code=zhongzheng1000_code,
+                              start_date=start_date, end_date=end_date)
+        sql = """
+            replace into tushare.index_weight
+            values (%s,%s,%s,%s)
+            """
+        insert_table_by_batch(sql, df.values.tolist())
+
+
+if __name__ == '__main__':
+    get_zhongzheng1000_history()
--- update_stock_data.py
+++ update_stock_data.py
@@ -4,8 +4,6 @@ import akshare as ak
 import easyquotation
 import retrying
 from mysql_util import get_connection, time_cost, get_max_date, insert_table_by_batch
-from slope_strategy import load_spark_sql, get_spark, get_ols, get_stock_best_parameter
-from pyspark.sql.types import StructType, DoubleType
 import time
 import pytz
 
@@ -90,197 +88,13 @@ def update_stock_history_data(full=False):
         list(tqdm.tqdm(executor.map(get_exchange_stock_data, codes), total=len(codes)))
 
 
-@time_cost
-def get_stock_slope():
-    spark_sql = load_spark_sql()
-    spark = get_spark()
-    schema = StructType().add("slope", DoubleType()).add("intercept", DoubleType()).add("r2", DoubleType())
-    spark.udf.register('get_ols', get_ols, schema)
-    with get_connection() as cursor:
-        sql = '''
-        select code, date, close
-        from stock.ods_stock_history
-        where date in (select date from etf.dim_etf_trade_date where rn<=600)
-        '''
-        cursor.execute(sql)
-        etf_df = cursor.fetchall()
-
-    etf_df = spark.createDataFrame(etf_df, ['code', 'date', 'close'])
-    etf_df.createOrReplaceTempView("df")
-    res = spark.sql(spark_sql[0])
-    update_min_date = get_max_date(n=5)
-    res = res.where(res.date >= update_min_date).toPandas()
-    res = res.fillna(0)
-    sql = '''
-    replace into stock.dws_stock_slope_history(code, `date`, close, slope, slope_mean, slope_std)
-    values (%s, %s, %s, %s, %s, %s)
-    '''
-    insert_table_by_batch(sql, res.values.tolist())
-
-
-@time_cost
-def get_stock_slope_rt():
-    max_rt_date = get_max_date()
-    spark_sql = load_spark_sql()
-
-    sql_rt = f'''
-    select t1.code, t1.date, (t1.increase_rate+1)*t2.close close
-    from (
-    select code, date, increase_rate
-    from stock.ods_stock_realtime
-    where date in (select date from etf.dim_etf_trade_date where rn=1)
-    )t1 
-    join (
-    select code, date, close 
-    from stock.ods_stock_history
-    where date in (select date from etf.dim_etf_trade_date where rn=2)
-    )t2
-    on t1.code=t2.code
-    union all
-    select code, date, close
-    from stock.ods_stock_history
-    where date in (select date from etf.dim_etf_trade_date where rn<=20) and date<'{max_rt_date}'
-    '''
-    with get_connection() as cursor:
-        cursor.execute(sql_rt)
-        etf_df = cursor.fetchall()
-    spark = get_spark()
-    schema = StructType().add("slope", DoubleType()).add("intercept", DoubleType()).add("r2", DoubleType())
-    spark.udf.register('get_ols', get_ols, schema)
-    etf_df = spark.createDataFrame(etf_df, ['code', 'date', 'close'])
-    etf_df.createOrReplaceTempView("df")
-    res = spark.sql(spark_sql[1].format(max_rt_date)).toPandas()
-    res = res.fillna(0)
-    sql = '''
-    replace into stock.dws_stock_slope_realtime(code, `date`, close, slope)
-    values (%s, %s, %s, %s)
-    '''
-    insert_table_by_batch(sql, res.values.tolist())
-
-
-def update_rt_rpt():
-    sql = '''
-    replace into stock.ads_stock_strategy_rt_rpt(code, name, date, scale, price, slope, last_slope, slope_low, slope_high, `signal`)
-    select t1.code,
-                 t1.name,
-                 t1.date,
-                 t1.scale,
-                 t1.price,
-                 t1.slope,
-                 t2.slope last_slope,
-                 t1.slope_low,
-                 t1.slope_high,
-                 case when t1.slope_low is null then NULL
-                            when t1.slope>=t1.slope_low and t2.slope<=t1.slope_low then 'buy'
-                            when t1.slope>=t1.slope_high then 'sell'
-                            else 'hold'
-                 end as `signal`
-                 
-    from (
-    select t1.code,
-                 t1.date,
-                 t2.name,
-                 t2.scale,
-                 t1.close price,
-                 (t1.slope-t4.slope_mean)/if(t4.slope_std=0, 1e-6, t4.slope_std)  slope,
-                 t5.low slope_low,
-                 t5.high slope_high
-    from stock.dws_stock_slope_realtime t1
-    join stock.dim_stock_basic_info t2 
-      on t1.code=t2.code
-    join stock.dim_stock_slope_standard_param t4 
-       on t1.code=t4.code
-    left join stock.dim_stock_slope_best t5
-      on t1.code=t5.code
-    where t1.date in (select date from etf.dim_etf_trade_date where rn=1)
-    ) t1
-    left join (
-    select code, date, (slope-slope_mean)/if(slope_std=0, 1e-6, slope_std)  slope
-    from stock.dws_stock_slope_history
-    where date in (select date from etf.dim_etf_trade_date where rn=2)
-    )t2
-    on t1.code=t2.code
-    '''
-    with get_connection() as cursor:
-        cursor.execute(sql)
-
-
-def update_history_rpt():
-    sql = '''
-    replace into stock.ads_stock_strategy_history_rpt(code, name, date, scale, price, slope, last_slope, slope_low, slope_high, `signal`)
-    select code,
-                 name,
-                 date,
-                 scale,
-                 price,
-                 slope,
-                 last_slope,
-                 slope_low,
-                 slope_high,
-                 case when slope>=slope_low and slope_low>=last_slope then 'buy'
-                            when slope>=slope_high then 'sell'
-                            else 'hold'
-                    end `signal`
-    from (
-    select t1.code,
-                 t1.name,
-                 t1.date,
-                 t1.scale,
-                 t1.price,
-                 t1.slope,
-                 lag(t1.slope, 1) over(partition by code order by date) last_slope,
-                 t1.slope_low,
-                 t1.slope_high,
-                 null `signal`
-    from (
-    select t1.code,
-                 t1.date,
-                 t2.name,
-                 t2.scale,
-                 t1.close price,
-                 (t1.slope-t4.slope_mean)/if(t4.slope_std=0, 1e-6, t4.slope_std) slope,
-                 t5.low slope_low,
-                 t5.high slope_high
-    from (select * from stock.dws_stock_slope_history 
-                 where date in (select date from etf.dim_etf_trade_date where rn<=2)) t1
-    join stock.dim_stock_basic_info t2 
-      on t1.code=t2.code
-    join stock.dim_stock_slope_standard_param t4 
-       on t1.code=t4.code
-    left join stock.dim_stock_slope_best t5
-      on t1.code=t5.code
-    ) t1
-    )t
-    where date in (select date from etf.dim_etf_trade_date where rn<=1)
-    '''
-    with get_connection() as cursor:
-        cursor.execute(sql)
-
-
-def update_std():
-    sql = '''
-    replace into stock.dim_stock_slope_standard_param(code, slope_mean, slope_std)
-    select code, slope_mean, slope_std
-    from stock.dws_stock_slope_history
-    where date in (select date from etf.dim_etf_trade_date where rn=2)
-    '''
-    with get_connection() as cursor:
-        cursor.execute(sql)
-
-
 def run_every_minute():
     update_stock_realtime()
-    get_stock_slope_rt()
-    update_rt_rpt()
 
 
 def run_every_day():
     update_stock_basic_info()
     update_stock_history_data()
-    get_stock_slope()
-    update_std()
-    get_stock_best_parameter()
-    update_history_rpt()
 
 
 if __name__ == "__main__":
