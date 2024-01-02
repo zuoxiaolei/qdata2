989a35dc57620fa98a743ff0730e55d680f819fb
zuoxiaolei <1905561110@qq.com>
2024-01-02 23:42:33 +0800

    添加收益更新提醒

--- send_message.py
+++ send_message.py
@@ -28,7 +28,7 @@ def send_ratation_message(last_day, last_day_profit):
     print({"last_day": last_day, "now": now})
     last_day_profit = round(last_day_profit, 2)
     hour = datetime.now(tz).hour
-    if last_day == now and 9 <= hour <= 16:
+    if last_day == now and 9 <= hour <= 15:
         message = f"组合投资\n日期：{last_day}\n涨幅：{last_day_profit}%"
         send_message(message)
 
